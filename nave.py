49d3eb6b2e26297b4b4028d857cc04b6f6f93d82
Talisson <talissonqueiroz@outlook.com>
2018-01-03 22:53:20 -0300

    USS Enterprise - D
    implemento da Class Nave
    implemento do Deck01

--- nave.py
+++ nave.py
@@ -1,2 +1,34 @@
+# USS ENTERPRISE - D  http://ussenterprised.blogspot.com.br
+
 class Nave:
-    turbina = True
+    ponte = True
+    engenharia_geral = True
+    computador = True
+    dobra = True
+    naceles = True
+    impulso = True
+    redes = True
+    comunicacoes = True
+    sistema_transporte = True
+    sensores = True
+    ciencia = True
+    sistema_tatico = True
+    sistema_ambiental = True
+    suporte_tripulantes = True
+    naves_auxiliares = True
+    operacoes_voo = True
+    emergencia = False
+
+class Deck01(Nave):
+    sistema_sensorial = True
+    orientacao_navegacao = True
+    engenharia = True
+    meio_ambiente = True
+    missao_ops = True
+    estacao_ciencia = True
+    estacao_comando = True
+
+
+
+
+

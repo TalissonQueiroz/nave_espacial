d6c56b1d7334b807d0471816065eb3cbf1336004
Talisson <talissonqueiroz@outlook.com>
2018-01-04 09:37:31 -0300

    Implemento Decks: 02,03,04,05,06,07,08

--- nave.py
+++ nave.py
@@ -9,7 +9,7 @@ class Nave:
     impulso = True
     redes = True
     comunicacoes = True
-    sistema_transporte = True
+    sistema_transporte_geral = True
     sensores = True
     ciencia = True
     sistema_tatico = True
@@ -27,7 +27,26 @@ class Deck01(Nave):
     missao_ops = True
     estacao_ciencia = True
     estacao_comando = True
-
+class Deck02(Nave):
+    gestao_operacoes = True
+    operacao_ponte = True
+    ponte_principal = True
+    quartos_oficiais = True
+class Deck03(Nave):
+    shuttebay_principal_superior = True
+class Deck04(Nave):
+    sistema_transporte_carga = True
+    shuttebay_principal = True
+class Deck05(Nave):
+    nucleo_computador = True
+    sessao_disco = True
+class Deck06_07(Nave):
+    operacao_transporte = True
+    sistema_transporte_pessoas = True
+    atmosfera_agua = True
+    nucleo_computador = True
+    quartos = True
+class Deck08(Nave):
 
 
 
